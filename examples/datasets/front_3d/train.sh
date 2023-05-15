a8bbf02809c96fb497146fd4a14dd7f8bf8ac44f
Mohamad <no.future2e1@gmail.com>
2023-05-15 15:44:43 +0300

    add train script

---
+++ examples/datasets/front_3d/train.sh
@@ -0,0 +1,11 @@
+#!bash
+pip install transformers
+pip install datasets
+pip install accelerate
+pip install wandb
+pip install git+https://github.com/huggingface/diffusers.git
+apt install unzip
+git clone https://github.com/huggingface/diffusers
+
+export $hf_token= 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
+export $wnb_token= c17faf7466a9921cb8fdb36b93d1d577615d7736
